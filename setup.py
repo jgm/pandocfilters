b9acd2f40f6f2399e75ded8b0a23b1c0fa9b311a
John MacFarlane <fiddlosopher@gmail.com>
2013-09-30 09:55:36 -0700

    Version bump to 1.0.1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='pandocfilters',
-      version='1.0',
+      version='1.0.1',
       description='Utilities for writing pandoc filters in python',
       long_description=read('README.rst'),
       author='John MacFarlane',
