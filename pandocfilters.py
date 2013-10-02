aa6d4900bbadfa1d86e25d91c3d628c5312923c7
John MacFarlane <fiddlosopher@gmail.com>
2013-10-02 09:08:39 -0700

    Added constructors for block and inline elements.
    This gives a level of abstraction, allowing us to change
    the underlying JSON representations without requiring changes
    to filters.

--- pandocfilters.py
+++ pandocfilters.py
@@ -93,3 +93,53 @@ def attributes(attrs):
   classes = attrs.get("classes",[])
   keyvals = [[x,attrs[x]] for x in attrs if (x != "classes" and x != "id")]
   return [ident, classes, keyvals]
+
+def elt(eltType, numargs):
+  def fun(*args):
+    lenargs = len(args)
+    if lenargs != numargs:
+      raise(eltType + ' expects ' + str(numargs) + ' arguments, but given ' +
+              str(lenargs))
+    if len(args) == 1:
+      xs = args[0]
+    else:
+      xs = args
+    return dict([(eltType, xs)])
+  return fun
+
+# Constructors for block elements
+
+Plain = elt('Plain',1)
+Para = elt('Para',1)
+CodeBlock = elt('CodeBlock',2)
+RawBlock = elt('RawBlock',2)
+BlockQuote = elt('BlockQuote',1)
+OrderedList = elt('OrderedList',2)
+BulletList = elt('BulletList',1)
+DefinitionList = elt('DefinitionList',1)
+Header = elt('Header',3)
+HorizontalRule = elt('HorizontalRule',0)
+Table = elt('Table',5)
+Div = elt('Div',2)
+Null = elt('Null',0)
+
+# Constructors for inline elements
+
+Str = elt('Str',1)
+Emph = elt('Emph',1)
+Strong = elt('Strong',1)
+Strikeout = elt('Strikeout',1)
+Superscript = elt('Superscript',1)
+Subscript = elt('Subscript',1)
+SmallCaps = elt('SmallCaps',1)
+Quoted = elt('Quoted',2)
+Cite = elt('Cite',2)
+Code = elt('Code',2)
+Space = elt('Space',0)
+LineBreak = elt('LineBreak',0)
+Math = elt('Math',2)
+RawInline = elt('RawInline',2)
+Link = elt('Link',2)
+Image = elt('Image',2)
+Note = elt('Note',1)
+Span = elt('Span',2)
