8c0b89e3f81704b152a2e277aa92fea03d54a636
August Hörandl <august.hoerandl@gmx.at>
2016-04-27 18:58:59 +0200

    abc: use utility functions

--- examples/abc.py
+++ examples/abc.py
@@ -6,48 +6,36 @@ ABC notation into images.  Assumes that abcm2ps and ImageMagick's
 convert are in the path.  Images are put in the abc-images directory.
 """
 
-import hashlib
 import os
 import sys
-from pandocfilters import toJSONFilter, Para, Image
 from subprocess import Popen, PIPE, call
 
-imagedir = "abc-images"
+from pandocfilters import toJSONFilter, Para, Image, get_caption, get_filename4code, get_extension
 
 
-def sha1(x):
-    return hashlib.sha1(x.encode(sys.getfilesystemencoding())).hexdigest()
-
-
-def abc2eps(abc, filetype, outfile):
+def abc2eps(abc_src, filetype, outfile):
     p = Popen(["abcm2ps", "-O", outfile + '.eps', "-"], stdin=PIPE)
-    p.stdin.write(abc)
+    p.stdin.write(abc_src)
     p.communicate()
     p.stdin.close()
     call(["convert", outfile + '.eps', outfile + '.' + filetype])
 
 
-def abc(key, value, format, meta):
+def abc(key, value, format, _):
     if key == 'CodeBlock':
         [[ident, classes, keyvals], code] = value
         if "abc" in classes:
-            outfile = imagedir + '/' + sha1(code)
-            if format == "html":
-                filetype = "png"
-            elif format == "latex":
-                filetype = "pdf"
-            else:
-                filetype = "png"
-            src = outfile + '.' + filetype
-            if not os.path.isfile(src):
-                try:
-                    os.mkdir(imagedir)
-                    sys.stderr.write('Created directory ' + imagedir + '\n')
-                except OSError:
-                    pass
+            caption, typef, keyvals = get_caption(keyvals)
+            outfile = get_filename4code("abc", code)
+            filetype = get_extension(format, "png", html="png", latex="pdf")
+            dest = outfile + '.' + filetype
+
+            if not os.path.isfile(dest):
                 abc2eps(code.encode("utf-8"), filetype, outfile)
-                sys.stderr.write('Created image ' + src + '\n')
-            return Para([Image(['', [], []], [], [src, ""])])
+                sys.stderr.write('Created image ' + dest + '\n')
+
+            return Para([Image([ident, [], keyvals], caption, [dest, typef])])
+
 
 if __name__ == "__main__":
     toJSONFilter(abc)
