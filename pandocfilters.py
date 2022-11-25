ed3a039d71d1b585c0148637748b300ee995b04e
Augusto Zanellato <augusto.zanellato@gmail.com>
2022-11-25 14:11:04 +0100

    Fix possible TOCTOU issue in directory creation

--- pandocfilters.py
+++ pandocfilters.py
@@ -12,7 +12,6 @@ import hashlib
 import io
 import json
 import os
-import os.path
 import sys
 import atexit
 import shutil
@@ -48,14 +47,11 @@ def get_filename4code(module, content, ext=None):
     else:
         imagedir = module + "-images"
     fn = hashlib.sha1(content.encode(sys.getfilesystemencoding())).hexdigest()
-    if os.path.exists(imagedir):
-        if not os.path.isdir(imagedir):
-            print(imagedir + " exists but it's not a directory", file=sys.stderr)
-    else:
-        try:
-            os.mkdir(imagedir)
-        except OSError:
-            print("Could not create directory " + imagedir, file=sys.stderr)
+    try:
+        os.makedirs(imagedir, exist_ok=True)
+        sys.stderr.write('Created directory ' + imagedir + '\n')
+    except OSError:
+        sys.stderr.write('Could not create directory "' + imagedir + '"\n')
     if ext:
         fn += "." + ext
     return os.path.join(imagedir, fn)
